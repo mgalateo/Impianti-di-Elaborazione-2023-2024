35da61581cc0209e37a154dff9d08ea5eb1eefa8
Mauro Galateo <ma.galateo@studenti.unina.it>
2023-11-27 17:53:07 +0100

    Test Normalità in matlab applicabile alle colonne principali automaticamente

--- Capacity2/Workload Carachterization/LL/testNormalita.m
+++ Capacity2/Workload Carachterization/LL/testNormalita.m
@@ -15,3 +15,25 @@ disp(data);
 % Puoi anche stampare i dati di testo
 disp('Dati di testo:');
 disp(text);
+
+
+for i = 1:9
+    % Accedi alla colonna
+    colonna = data(:, i);
+    % Esegui il test di Lilliefors
+    [h, p, stat] = lillietest(colonna);
+    
+    % Visualizza i risultati
+    disp(['componente n ', i])
+    disp (i)
+    disp(['H0: I dati provengono da una distribuzione normale']);
+    disp(['p-value: ', num2str(p)]);
+    disp(['Statistiche del test: ', num2str(stat)]);
+    if h
+        disp('Il test rigetta l ipotesi nulla a livello di significatività del 5%.');
+    else
+        disp('Il test non rigetta l ipotesi nulla a livello di significatività del 5%.');
+        disp('La componente Principale è NORMALE')
+    end
+end
+
