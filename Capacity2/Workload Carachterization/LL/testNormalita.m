362788a9fbf2d44860428a8e3f7ba19c47b74d3c
Mauro Galateo <ma.galateo@studenti.unina.it>
2023-11-28 18:12:08 +0100

    modifica cartella test

--- Capacity2/Workload Carachterization/LL/testNormalita.m
+++ Capacity2/Workload Carachterization/LL/testNormalita.m
@@ -1,20 +1,12 @@
+clear;
+clc;
+
 % Specifica il percorso del file XLSX
 file_path = 'LLc.xlsx';
 
 % Leggi i dati dal file XLSX
 [data, text, raw] = xlsread(file_path);
 
-% 'data' contiene i dati numerici
-% 'text' contiene i dati di testo
-% 'raw' contiene tutti i dati nel formato originale del foglio di lavoro
-
-% Ad esempio, puoi stampare i dati numerici
-disp('Dati numerici:');
-disp(data);
-
-% Puoi anche stampare i dati di testo
-disp('Dati di testo:');
-disp(text);
 
 componentiNormali=[]
 
